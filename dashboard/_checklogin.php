1984fd1ef2fba90db2cf3d0a999994969d63cd89
_Fida_Talbia_190216 <166204885+FidaTalbiaBeginner@users.noreply.github.com>
2024-05-30 04:27:31 -0600

    Add files via upload

---
+++ dashboard/_checklogin.php
@@ -0,0 +1,11 @@
+<?php
+  if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $email = $_POST['email'];
+    setcookie("admin", $email, time() + (86400 * 365), '/');
+    header("location: dashboard.php");
+  }
+  else{
+    setcookie("admin", $email, time() - (86400 * 365), '/');
+    header("location: login.php");
+  }
+?>
